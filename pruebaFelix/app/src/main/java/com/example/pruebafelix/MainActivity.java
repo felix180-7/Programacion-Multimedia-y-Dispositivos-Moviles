9a18dc268ee1b4a88af7f7a2d5a7742610718e33
Felix <felix_cmartin@alumnos-santagema.es>
2025-10-21 18:54:53 +0200

    texto blanco

--- pruebaFelix/app/src/main/java/com/example/pruebafelix/MainActivity.java
+++ pruebaFelix/app/src/main/java/com/example/pruebafelix/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.reg);
+        setContentView(R.layout.login);
     }
 }
