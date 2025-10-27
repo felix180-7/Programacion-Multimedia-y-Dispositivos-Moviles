ae1954576e3774aa08fde3c4923101e649a24380
Felix <felix_cmartin@alumnos-santagema.es>
2025-10-27 20:39:19 +0100

    textos

--- EditText/app/src/main/java/com/example/edittext/MainActivity.java
+++ EditText/app/src/main/java/com/example/edittext/MainActivity.java
@@ -1,9 +1,12 @@
-package com.example.pruebafelix;
+package com.example.edittext;
 
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.login
-        );
+        setContentView(R.layout.activity_main);
     }
 }
