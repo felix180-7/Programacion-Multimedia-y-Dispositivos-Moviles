d3ce070ded0dc5c49aab5a44661243a3b26e56d1
Felix <felix_cmartin@alumnos-santagema.es>
2025-11-05 20:42:44 +0100

    tema 5 parte listView

---
+++ AdapatdoresPersonalizados/app/src/main/java/com/example/adapatdorespersonalizados/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.adapatdorespersonalizados;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.principal);
+
+        Datos [] datos = new Datos [] {
+                new Datos("Linea principal 1 ","Linea inferior 1"),
+                new Datos("Linea principal 2 ","Linea inferior 2"),
+                new Datos("Linea principal 3 ","Linea inferior 3"),
+                new Datos("Linea principal 4 ","Linea inferior 4")
+        };
+
+        ListView a = findViewById(R.id.list);
+
+        Adaptador adaptador = new Adaptador(this,datos);
+
+        a.setAdapter(adaptador);
+    }
+
+}
