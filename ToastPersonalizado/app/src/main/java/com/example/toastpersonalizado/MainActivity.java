485743196edc728ac9bd86a84230002405dd49eb
Felix <felix_cmartin@alumnos-santagema.es>
2026-01-13 15:07:31 +0100

    toast

---
+++ ToastPersonalizado/app/src/main/java/com/example/toastpersonalizado/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.toastpersonalizado;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnToast).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = getLayoutInflater();
+                View customView = inflater.inflate(R.layout.lay2,null);
+
+                Toast toast = new Toast(getApplicationContext());
+                toast.setView(customView);
+                toast.setDuration(Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER_VERTICAL,0,100);
+                toast.show();
+            }
+        });
+    }
+}
