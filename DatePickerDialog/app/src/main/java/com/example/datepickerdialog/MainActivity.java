b58101ba2ae9187ae433f7337b7c2cf4e5af71dd
Felix <felix_cmartin@alumnos-santagema.es>
2026-01-13 20:39:45 +0100

    a

--- DatePickerDialog/app/src/main/java/com/example/datepickerdialog/MainActivity.java
+++ DatePickerDialog/app/src/main/java/com/example/datepickerdialog/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.datepickerdialog;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,43 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button b = findViewById(R.id.aaa);
+        TextView c = findViewById(R.id.bbb);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog(c);
+            }
         });
+
+    }
+
+    private void showDatePickerDialog(TextView resultTextView) {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+        this,
+                (view, selectedYear, selectedMonth, selectedDay) -> {
+            String selectedDate = selectedDay + "/" + (selectedMonth + 1) +"/" + selectedYear;
+            resultTextView.setText("Fecha seleccionada: "+ selectedDate);
+                },
+            year, month, day
+        );
+
+        datePickerDialog.show();
     }
 }
